b04863747d3da6761cfb58178a11e6b4ac07ffdc
Noilty (Roma Kuzmin) <thenoilty@gmail.com>
2019-12-10 22:40:57 +0700

    Добавил разметку

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
-<?php
+<!DOCTYPE html>
+<html lang="ru">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,minimum-scale=1,maximum-scale=10">
+        <title>WOT: SFS</title>    
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    
+    <body style="text-align: center;">
+        
+        <div class="content-wrapper">
+            
+            <header class="header">
+                <p style="font-size: 30px; margin: 0; padding: 10px 1em;">{HEADER}</p>
+            </header>
 
-echo 'Hello, WOlrd!';
+            <div class="container clearfix">
+                
+                <!-- DB [Center] -->
+                <main class="content">
+                    <p style="text-align: center; font-size: 30px; margin: 0; padding: 1.5em 0;">{DB}</p>
+                </main>
+                <!-- / DB [Center] -->
+                
+                <!-- INPUT [left] -->
+                <aside class="sidebar sidebar-left">
+                    <p style="text-align: center;">{INPUT}</p>
+                </aside>
+                <!-- INPUT [left] -->
+                
+                <!-- SOURCE [right] -->
+                <aside class="sidebar sidebar-right">
+                    <p style="text-align: center;">{SOURCE}</p>
+                </aside>
+                <!-- / SOURCE [right] -->
+                
+            </div>
+
+            <footer class="footer">
+                <p style="font-size: 30px; margin: 0; padding: 10px 1em;">{FOOTER}</p>
+            </footer>
+            
+        </div>
+        
+    </body>
+    
+</html>
