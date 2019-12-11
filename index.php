89afb87e3b946cd944db72e5b4d1fba1fd26d93e
Noilty <thenoilty@gmail.com>
2019-12-11 22:59:29 +0700

    Поправил разметку добавил навигацию

--- index.php
+++ index.php
@@ -8,12 +8,53 @@
         <link rel="stylesheet" href="css/style.css">
     </head>
     
-    <body style="text-align: center;">
+    <body>
         
         <div class="content-wrapper">
             
-            <header class="header">
-                <p style="font-size: 30px; margin: 0; padding: 10px 1em;">{HEADER}</p>
+            <header class="grid">
+                
+                <div class="grid__item">
+                    <ul>
+                        <a href="#">
+                            <li>Главная</li>
+                        </a>
+                        <a href="#" title="Browser Source (OBS)" target="_blank">
+                            <li>Источник</li>
+                        </a>
+                        <li>
+                            Центр поддержки
+                            <ul>
+                                <a href="#">
+                                    <li>Мои заявки</li>
+                                </a>
+                                <a href="#">
+                                    <li>Мои баны</li>
+                                </a>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                
+                <div class="grid__item">
+                    <ul>
+                        <li>
+                            С возвращением <strong>Noilty</strong>!
+                            <ul>
+                                <a href="#">
+                                    <li>Активировать код</li>
+                                </a>
+                                <a href="#">
+                                    <li>Настройки</li>
+                                </a>
+                                <a href="#">
+                                    <li>Выход</li>
+                                </a>
+                            </ul>
+                        </li>                        
+                    </ul>
+                </div>
+                
             </header>
 
             <div class="container clearfix">
@@ -38,11 +79,17 @@
                 
             </div>
 
-            <footer class="footer">
+            <footer>
                 <p style="font-size: 30px; margin: 0; padding: 10px 1em;">{FOOTER}</p>
             </footer>
             
         </div>
+        
+        <!-- jQuery mini -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>        
+        <!-- / jQuery mini -->
         
     </body>
     
