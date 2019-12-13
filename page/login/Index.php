532aa8b32f0dd2aed9450df71d5196a8eac7b882
Noilty <thenoilty@gmail.com>
2019-12-14 01:29:53 +0700

    Сделал макет для страницы входа на сайт

---
+++ page/login/Index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="ru">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,maximum-scale=10">
+    <title>Log In - WOT: BSFS</title>    
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="shortcut icon" href="../../favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+    
+    <div class="content-wrapper">
+        
+        <!-- Шапка -->
+        <?php require_once '../../assets/connect/header/index.php'; ?>
+        <!-- / Шапка -->
+        
+        <!-- Меню навигации -->
+        <nav class="grid-nav">
+            <!-- Левая часть меню -->
+            <div class="grid-nav__item">
+                <ul>
+                    <a href="#">
+                        <li>Главная</li>
+                    </a>
+                </ul>
+            </div>
+            <!-- / Левая часть меню -->
+
+            <!-- Правая часть меню -->
+            <div class="grid-nav__item">
+                <ul>
+                    <a id="login-to-continue">
+                        <li>Войдите чтобы продолжить!</li>
+                    </a>
+                </ul>
+            </div>
+            <!-- / Правая часть меню -->            
+        </nav>
+        <!-- / Меню навигации -->
+
+        <!-- Здесь начинается контент -->
+        <div id="form-login" class="grid-content" style="display: none;" align="center">
+            <div style="padding: 20px 450px 20px 450px;">
+                <form action="" method="POST">
+                    <fieldset>
+                        <legend class="legend-head" align="center">Вход</legend>
+                        <fieldset>
+                            <legend>Логин / Почта</legend>
+                            <input type="text" name="inputLogin" />
+                        </fieldset>
+                        <fieldset>
+                            <legend>Пароль</legend>
+                            <input type="text" name="inputPassword" />
+                        </fieldset>
+                        <fieldset>
+                            <legend>Проверка на бота</legend>
+                            <input type="text" name="inputNotBot" />
+                        </fieldset>
+                        <input type="submit" name="submitLogin" />
+                    </fieldset>                    
+                </form>
+                <a href="/wotbsfs/page/signup/" style="color: #000;">Нет аккаунта?</a>
+            </div>
+        </div>
+        <!-- / Здесь начинается контент -->
+        
+        <!-- Подвал -->
+        <?php require_once '../../assets/connect/footer/index.php'; ?>
+        <!-- / Подвал -->
+
+    </div>
+
+    <!-- jQuery mini -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>        
+    <!-- / jQuery mini -->
+
+    <!-- Скрипт плавного открытия и закрытия блока -->
+    <script>
+        $('#login-to-continue').click(function(){
+            $('#form-login').css('display','block');
+        });     
+    </script>   
+    
+</body>
+    
+</html>
