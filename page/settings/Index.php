4e2bcc6c2c539bda649e2f4aec46ad70046f0e32
Noilty <thenoilty@gmail.com>
2019-12-14 22:00:14 +0700

    Сделал макет страницы settings, поправил таблицу стилей

--- page/settings/Index.php
+++ page/settings/Index.php
@@ -9,7 +9,7 @@
     <link rel="shortcut icon" href="../../favicon.ico" type="image/x-icon">
 </head>
 
-<body>
+<body style="visibility: hidden" onload="setTimeout ('document.body.style.visibility = \'visible\'', 0)">    
     
     <div class="content-wrapper">
         
@@ -22,54 +22,167 @@
         <!-- / Меню навигации -->
 
         <!-- Здесь начинается контент -->
-        <div class="grid-content">
-            <div class="grid-content__item">
-                <div class="info-battle">
-                    <form action="" method="POST">
-                         <fieldset>
-                             <legend class="legend-head" title="Browser Source (OBS)" align="center">Источник</legend>
-                             <fieldset>
-                                 <legend>Количество</legend>
-                                 <fieldset>
-                                     <legend>Игроков</legend>
-                                     <select name="selectCountPlayers">
-                                         <option value="1">Один</option>
-                                         <option value="2">Два</option>
-                                         <option value="3">Три</option>
-                                     </select>
-                                 </fieldset>
-                                 <fieldset>
-                                     <legend>Элементов</legend>
-                                     <select name="selectCountElements">
-                                         <option value="500">500</option>
-                                         <option value="250">250</option>
-                                         <option value="100">100</option>
-                                         <option value="50">50</option>
-                                     </select>
-                                 </fieldset>
-                             </fieldset>
-                             <fieldset>
-                                 <legend>Шаблон</legend>
-                                 <select name="selectTemplate">
-                                     <option value="0">Noilty</option>
-                                     <option value="1">HINCO</option>
-                                     <option value="2">GoOgle</option>
-                                     <option value="3">HigGA</option>
-                                 </select>
-                             </fieldset>
-                             <hr />
-                             <fieldset>
-                                 <legend>Проверка на бота</legend>
-                                 <input type="text" name="inputNotBot" />
-                             </fieldset>
-                             <input type="submit" name="inputSettingSource" />
-                         </fieldset>                    
-                     </form>                    
+        <div style="color: #000; padding: 10px 10px 0px 10px;">
+            <fieldset>
+                <legend class="legend-head" align="center">Информация</legend>
+                <div class="grid-content">
+                    <div class="grid-content__item">
+                        <div class="info-battle">
+                            <fieldset>
+                                <legend class="legend-head" title="Browser Source (OBS)" align="center">Источник</legend>
+                                Количество Игроков: <strong>Один</strong><br />
+                                Количество Элементов: <strong>500</strong><br />
+                                <hr />
+                                Никнейм Игрок #1: <strong>NaN</strong><br />
+                                Никнейм Игрок #2: <strong>NaN</strong><br />
+                                Никнейм Игрок #3: <strong>NaN</strong><br />
+                            </fieldset>
+                        </div>
+                    </div>
+                    <div class="grid-content__item">
+                        <div class="info-db">
+                            <fieldset>
+                                <legend class="legend-head" align="center">Шаблон</legend>
+                                Идентификатор: <strong>#1</strong><br />
+                                Имя: <strong>Noilty</strong><br />
+                                Ссылка на автора: <strong>vk.com/romakuzmin</strong><br />
+                            </fieldset>
+                        </div>
+                    </div>
+                    <div class="grid-content__item">
+                        <div class="info-battle">
+                            <fieldset>
+                                <legend class="legend-head" align="center">Личное</legend>
+                                Идентификатор: <strong>#1</strong><br />
+                                Никнейм: <strong>Noilty</strong><br />
+                                Электронная почта: <strong>thenoilty@gmail.com</strong><br />
+                                <hr />
+                                Реальное имя: <strong>Рома</strong><br />
+                                Секретное слово: <strong>FoMainBitch</strong><br />
+                                Дата рождения: <strong>05.04.2019</strong>
+                                <hr />
+                                Дата регистрации: <strong>05.05.2019</strong><br />
+                                Дата последнего посещения: <strong>05.05.2019</strong><br />
+                            </fieldset>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </fieldset>
+        </div>
+        
+        <div style="color: #000; padding: 10px 10px 0px 10px;">
+            <fieldset>
+                <legend class="legend-head" align="center">Настройки</legend>
+                <div class="grid-content">
+                    <div class="grid-content__item">
+                        <div class="info-battle">
+                            <form id="formSettingSource" action="" method="POST">
+                                <fieldset>
+                                    <legend class="legend-head" title="Browser Source (OBS)" align="center">Источник</legend>
+                                    <fieldset>
+                                        <legend>Количество</legend>
+                                        <fieldset>
+                                            <legend>Игроков</legend>
+                                            <select name="select_CountPlayer">
+                                                <option value="1">Один</option>
+                                                <option value="2">Два</option>
+                                                <option value="3">Три</option>
+                                            </select>
+                                        </fieldset>
+                                        <fieldset>
+                                            <legend>Элементов</legend>
+                                            <select name="select_CountElement">
+                                                <option value="500">500</option>
+                                                <option value="250">250</option>
+                                                <option value="100">100</option>
+                                                <option value="50">50</option>
+                                            </select>
+                                        </fieldset>
+                                    </fieldset>
+                                    <fieldset>
+                                        <legend>Никнейм</legend>
+                                        <fieldset>
+                                            <legend>Игрок #1</legend>
+                                            <input type="text" name="inputNickNamePlayer1" />
+                                        </fieldset>
+                                        <fieldset>
+                                            <legend>Игрок #2</legend>
+                                            <input type="text" name="inputNickNamePlayer2" />
+                                        </fieldset>
+                                        <fieldset>
+                                            <legend>Игрок #3</legend>
+                                            <input type="text" name="input_NickNamePlayer3" />
+                                        </fieldset>
+                                    </fieldset>
+                                    <hr />
+                                    <fieldset>
+                                        <legend>Проверка на бота</legend>
+                                        <input type="text" name="input_NotBot" />
+                                    </fieldset>
+                                    <input type="submit" name="input_SettingSource" />
+                                </fieldset>                    
+                            </form>                    
+                        </div>
+                    </div>
+
+                    <div class="grid-content__item">
+                        <div class="info-db">
+                            <form id="formSettingTemplate" action="" method="POST">
+                                <fieldset>
+                                    <legend class="legend-head" align="center">Шаблон</legend>
+                                    <select name="select_Template">
+                                        <option value="0">Noilty</option>
+                                        <option value="1">HINCO</option>
+                                        <option value="2">GoOgle</option>
+                                        <option value="3">HigGA</option>
+                                    </select>
+                                    <hr />
+                                    <fieldset>
+                                        <legend>Проверка на бота</legend>
+                                        <input type="text" name="input_NotBot" />
+                                    </fieldset>
+                                    <input type="submit" name="input_SettingSource" />
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="grid-content__item">
+                        <div class="info-battle">
+                            <form id="formSettingPersonal" action="" method="POST">
+                                <fieldset>
+                                    <legend class="legend-head" align="center">Личное</legend>
+                                    <fieldset>
+                                        <legend>Реальное имя</legend>
+                                        <input type="text" name="input_UserName" />
+                                    </fieldset>
+                                    <fieldset>
+                                        <legend>Секретное слово</legend>
+                                        <input type="text" name="input_UserSecretWord" />
+                                        <fieldset>
+                                            <legend>Пароль</legend>
+                                            <input type="password" name="input_UserPassword" />
+                                        </fieldset>
+                                    </fieldset>
+                                    <fieldset>
+                                        <legend>Дата рождения</legend>
+                                        <input type="date" name="input_UserName" />
+                                    </fieldset>
+                                    <hr />
+                                    <fieldset>
+                                        <legend>Проверка на бота</legend>
+                                        <input type="text" name="input_NotBot" />
+                                    </fieldset>
+                                    <input type="submit" name="input_SettingSource" />
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+                </div>                
+            </fieldset>
         </div>
         <!-- / Здесь начинается контент -->
-        
+       
         <!-- Подвал -->
         <?php require_once '../../assets/connect/footer/index.php'; ?>
         <!-- / Подвал -->
@@ -88,6 +201,7 @@
         $( document ).ready(function() {
             $("#activation-code").mask("aaaa-aaaa-aaaa-aaaa");
             $('#activation-code').css('text-transform','uppercase');
+            $('.grid-content').css('grid-template-columns','1fr 1fr 1fr');
         });// document ready
     </script>    
 
